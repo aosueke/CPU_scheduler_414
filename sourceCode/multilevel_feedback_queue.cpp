4e8e8ff51ce6b5b6dca4f8ce951ffbdbc05449f3
aosueke <91437037+aosueke@users.noreply.github.com>
2024-11-17 16:52:59 -0500

    Update multilevel_feedback_queue.cpp

--- sourceCode/multilevel_feedback_queue.cpp
+++ sourceCode/multilevel_feedback_queue.cpp
@@ -1,3 +1,9 @@
-#include "multilevel_feedback_queue.h"
+#include "MLFQ.h"
 #include <iostream>
 
+Multilevel_Feedback_Queue::Multilevel_Feedback_Queue(int tq1, int tq2) : timeQuantum1(tq1), timeQuantum2(tq2) {}
+
+void Multilevel_Feedback_Queue::addProcess(const Process& new_process) {
+    All_processes.push_back(new_process);
+    que1.push(&All_processes.back());
+}
