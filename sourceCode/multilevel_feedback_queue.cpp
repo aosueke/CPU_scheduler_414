0e978a707c365700330a87e2ae18b71ed91d94ad
aosueke <91437037+aosueke@users.noreply.github.com>
2024-11-17 23:12:12 -0500

    Update multilevel_feedback_queue.cpp

--- sourceCode/multilevel_feedback_queue.cpp
+++ sourceCode/multilevel_feedback_queue.cpp
@@ -7,3 +7,18 @@ void Multilevel_Feedback_Queue::addProcess(const Process& new_process) {
     All_processes.push_back(new_process);
     que1.push(&All_processes.back());
 }
+
+void Multilevel_Feedback_Queue::run() {
+    int current_time = 0;
+
+    auto execute_Queue = [&](std::queue<Process*>& que, int time_quantum) {
+        int quantum_used = 0;
+
+        while (!queue.empty()) {
+            Process* process = que.front();
+            que.pop();
+
+            // Recording response_time of first execution
+            if (process->response_time == -1) {
+                process->response_time = current_time - process->arrival_time;
+            }
