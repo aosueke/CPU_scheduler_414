df18cd893989e35123f36a968edbeafa886854b7
aosueke <91437037+aosueke@users.noreply.github.com>
2024-11-18 19:12:00 -0500

    Update multilevel_feedback_queue.cpp

--- sourceCode/multilevel_feedback_queue.cpp
+++ sourceCode/multilevel_feedback_queue.cpp
@@ -11,14 +11,33 @@ void Multilevel_Feedback_Queue::addProcess(const Process& new_process) {
 void Multilevel_Feedback_Queue::run() {
     int current_time = 0;
 
-    auto execute_Queue = [&](std::queue<Process*>& que, int time_quantum) {
+    auto execute_Queue = [&](std::queue<Process*>& que, int timeQuantum) {
         int quantum_used = 0;
 
-        while (!queue.empty()) {
-            Process* process = que.front();
+        while (!que.empty()) {
+            Process* proc = que.front();
             que.pop();
 
             // Recording response_time of first execution
-            if (process->response_time == -1) {
-                process->response_time = current_time - process->arrival_time;
+            if (proc->response_time == -1) {
+                proc->response_time = current_time - proc->arrival_time;
             }
+            
+            int execTime = std::min(timeQuantum, proc->remaining_time); // execution time
+            proc->remaining_time -= execTime;
+            current_time += execTime;
+
+            if (proc->remaining_time == 0) {
+                proc->completed = true;
+                proc->turnaround_time = current_time - proc->arrival_time;
+                proc->waiting_time = proc->turnaround_time - proc->burst_time;
+            } else {
+                // Move to the next queue
+                if (que == que1) {
+                    que2.push(proc);
+                } else if (que == que2) {
+                    que3.push(proc);
+                }
+            }
+        }
+    };
