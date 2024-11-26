d9294446532b45dabe2d9ac3bda557d48092bf64
DKerr91 <jay.kerr01@gmail.com>
2024-11-26 09:22:04 -0500

    Create multilevel_queue.h

---
+++ sourceCode/multilevel_queue.h
@@ -0,0 +1,18 @@
+#ifndef MULTILEVEL_QUEUE_H
+#define MULTILEVEL_QUEUE_H
+
+#include "process.h"
+#include <vector>
+
+class MultilevelQueueScheduler {
+private:
+  std::vector<Process> queue1;
+  std::vector<Process> queue2;
+
+public:
+  void addProcess(const Process& process, int queue_level);
+  void run();
+  void printStats90:
+};
+
+#endif
