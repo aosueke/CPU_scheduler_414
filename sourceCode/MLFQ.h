1499ac59bc8812892f717d41f2cd943c486d4c49
aosueke <91437037+aosueke@users.noreply.github.com>
2024-12-05 20:34:57 -0500

    Update MLFQ.h

--- sourceCode/MLFQ.h
+++ sourceCode/MLFQ.h
@@ -9,14 +9,14 @@ class Multilevel_Feedback_Queue{
 private:
     int tq1; // Queue 1 time quantum
     int tq2; // Queue 2 time quantum
-    std::queue<Process*> que1; // high priority queue
-    std::queue<Process*> que2; // med priority queue
-    std::queue<Process*> que3; // low priority queue
-    std::vector<Process> All_processes;
+    std::queue<ProcessInfo*> que1; // high priority queue
+    std::queue<ProcessInfo*> que2; // med priority queue
+    std::queue<ProcessInfo*> que3; // low priority queue
+    std::vector<ProcessInfo> All_processes;
 
 public:
     Multilevel_Feedback_Queue(int tq1, int tq2);
-    void addProcess(const Process& new_process);
+    void addProcess(const ProcessInfo& new_process);
     void run();
     void printProcStats();
 };
